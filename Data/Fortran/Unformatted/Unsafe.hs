206a5053174aa8e860483f22f7999880e0ecc245
Alberto Valverde <alberto@meteogrid.com>
2014-02-12 22:56:08 +0100

    optimized Unsafe.fromUnformatted

--- Data/Fortran/Unformatted/Unsafe.hs
+++ Data/Fortran/Unformatted/Unsafe.hs
@@ -1,30 +1,30 @@
 module Data.Fortran.Unformatted.Unsafe (toUnformatted , fromUnformatted) where
     
-import Data.Monoid (mconcat)
+import Prelude hiding (splitAt)
+import Data.Monoid
 import Data.Maybe (fromMaybe)
-import Data.ByteString.Lazy as BS
-import Data.ByteString.Builder
-
+import Data.ByteString.Lazy (ByteString, uncons, splitAt, toStrict)
+import Data.ByteString.Builder (Builder, byteString, toLazyByteString)
 
 fromUnformatted :: ByteString -> ByteString
-fromUnformatted = toLazyByteString . mconcat . blocks . skipBOF
+fromUnformatted = toLazyByteString . blocks . skipBOF
   where
-    blocks :: ByteString -> [Builder]
+    blocks :: ByteString -> Builder
     blocks s = let (header,tail') = uncons' s
-                   (block',rest') = BS.splitAt (min 128 blockSize) tail'
+                   (block',rest') = splitAt (min 128 blockSize) tail'
                    atEof          = blockSize == 130
                    blockSize      = fromIntegral header
                    block          = if header == trailer
-                                    then lazyByteString block'
+                                    then byteString $ toStrict block'
                                     else error "fromUnformatted: Bad trailer"
                    (trailer,rest) = uncons' rest'
-               in if atEof then [] else block : blocks rest
-    skipBOF s = if fst (uncons' s) == 75
-                then snd (uncons' s)
+               in if atEof then mempty else block `seq` block <> blocks rest
+    skipBOF s = let (bof,rest) = uncons' s
+                in if bof == 75
+                then rest
                 else error "fromUnformatted: Invalid file. BOF /= 75"
-    uncons' = fromMaybe (error "fromUnformatted: unexpected EOF") . BS.uncons
-
-
+    uncons' = fromMaybe (error "fromUnformatted: unexpected EOF") . uncons
+{-# INLINEABLE fromUnformatted #-}
 
 toUnformatted :: ByteString -> ByteString
 toUnformatted = undefined

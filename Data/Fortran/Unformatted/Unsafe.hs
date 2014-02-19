d1c391dc8df9dee311d53c12a47ec9b87b086cc0
Alberto Valverde <alberto@meteogrid.com>
2014-02-19 09:30:31 +0100

    no need for Builder, BS.Lazy.concat is equally effective in this case

--- Data/Fortran/Unformatted/Unsafe.hs
+++ Data/Fortran/Unformatted/Unsafe.hs
@@ -1,24 +1,21 @@
 module Data.Fortran.Unformatted.Unsafe (toUnformatted , fromUnformatted) where
     
-import Prelude hiding (splitAt)
-import Data.Monoid
+import Prelude hiding (concat, splitAt)
 import Data.Maybe (fromMaybe)
-import Data.ByteString.Lazy (ByteString, uncons, splitAt, toStrict)
-import Data.ByteString.Builder (Builder, byteString, toLazyByteString)
+import Data.ByteString.Lazy (ByteString, uncons, splitAt, concat)
 
 fromUnformatted :: ByteString -> ByteString
-fromUnformatted = toLazyByteString . blocks . skipBOF
+fromUnformatted = concat . blocks . skipBOF
   where
-    blocks :: ByteString -> Builder
     blocks s = let (header,tail') = uncons' s
                    (block',rest') = splitAt (min 128 blockSize) tail'
                    atEof          = blockSize == 130
                    blockSize      = fromIntegral header
                    block          = if header == trailer
-                                    then byteString $ toStrict block'
+                                    then block'
                                     else error "fromUnformatted: Bad trailer"
                    (trailer,rest) = uncons' rest'
-               in if atEof then mempty else block `seq` block <> blocks rest
+               in if atEof then [] else block `seq` block : blocks rest
     skipBOF s = let (bof,rest) = uncons' s
                 in if bof == 75
                 then rest
